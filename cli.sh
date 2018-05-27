fb5f92c6d8694719333a74b2b1899055a77390d4
Felix Klein <felix@felixklein.net>
2018-05-27 22:31:03 +0200

    no commit message

--- cli.sh
+++ cli.sh
@@ -16,7 +16,8 @@ export TF_SKIP_CONTRIB_TESTS=true
 
 case "${1:-}" in
 deps)
-  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+  sudo -v
+  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
   distribution=$(. /etc/os-release echo $ID$VERSION_ID)
   curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
   sudo apt-get update
