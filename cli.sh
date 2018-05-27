d7dc65f992f3c2019ad639ae23ae3ac02fd2dad7
Felix Klein <felix@felixklein.net>
2018-05-27 23:24:49 +0200

    no commit message

--- cli.sh
+++ cli.sh
@@ -17,11 +17,16 @@ export TF_SKIP_CONTRIB_TESTS=true
 case "${1:-}" in
 deps)
   sudo -v
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  curl -fsSL 'https://download.docker.com/linux/ubuntu/gpg' | sudo apt-key add -
   curl -s -L 'https://nvidia.github.io/nvidia-docker/gpgkey' | sudo apt-key add -
-  curl -s -L "https://nvidia.github.io/nvidia-docker/$(source /etc/os-release; echo $ID$VERSION_ID)/nvidia-docker.list" | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+  curl -s -L "https://nvidia.github.io/nvidia-docker/$(
+    source /etc/os-release
+    echo $ID$VERSION_ID
+  )/nvidia-docker.list" | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
   sudo apt-get update
-  sudo apt-get install -y nvidia-docker2
-  sudo pkill -SIGHUP dockerd
+  sudo apt-get install -y docker-ce nvidia-docker2
+  sudo systemctl restart docker
   ;;
 init)
   # initialize src
