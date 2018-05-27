2e13a184120fa8a4a1dc403d8f4a07a40061eb05
Felix Klein <felix@felixklein.net>
2018-05-27 22:37:28 +0200

    no commit message

--- cli.sh
+++ cli.sh
@@ -18,8 +18,7 @@ case "${1:-}" in
 deps)
   sudo -v
   curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-  distribution=$(. /etc/os-release echo $ID$VERSION_ID)
-  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+  curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu18.04/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
   sudo apt-get update
   sudo apt-get install -y nvidia-docker2
   sudo pkill -SIGHUP dockerd
